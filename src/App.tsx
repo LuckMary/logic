1d1b0add5831cc53b5cb06f80e9b7371eedf0c9f
Mary Lukicheva <luckmary8@gmail.com>
2024-05-28 17:01:16 +0300

    code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -3,62 +3,53 @@ import axios from "axios";
 
 import "./App.scss";
 
+interface ICourse {
+  id: string;
+  name: string;
+  image: string;
+  bgColor: string;
+  tags: string[];
+}
+
 function App() {
-  const [courses, setCourses] = useState<any[]>([]);
-  const [selectedTag, setSelectedTag] = useState("Все темы");
-  const [selectedCourses, setSelectedCourses] = useState<any[]>([]);
+  const [courses, setCourses] = useState<ICourse[]>([]);
+  const [tags, setTags] = useState<string[]>([]);
+  const [selectedTag, setSelectedTag] = useState<string>();
 
   useEffect(() => {
-    getAPI();
+    getCourses();
   }, []);
 
-  const getAPI = async () => {
+  const getCourses = async () => {
     try {
-      const response = await axios.request({
-        method: "GET",
-        url: "https://logiclike.com/docs/courses.json",
-      });
-      setCourses(response.data);
-      setSelectedCourses(response.data);
+      const { data } = await axios.get<ICourse[]>(
+        "https://logiclike.com/docs/courses.json"
+      );
+
+      setCourses(data);
+
+      setTags([...new Set(data.map((course) => course.tags).flat())]);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const tags = [
-    "Все темы",
-    "Логика и мышление",
-    "Загадки",
-    "Головоломки",
-    "Путешествия",
-  ];
-
-  const rows: any[] = selectedCourses
-    .map(
-      (course, index) =>
-        index % 3 === 0 && selectedCourses.slice(index, index + 3)
-    )
-    .filter(Boolean);
-
   return (
     <div className="App">
       <ul>
+        <li
+          onClick={() => {
+            setSelectedTag("");
+          }}
+          {...(!selectedTag && { className: "selected" })}
+        >
+          Все темы
+        </li>
         {tags.map((tag) => (
           <li
             key={tag}
             onClick={() => {
               setSelectedTag(tag);
-              setSelectedCourses(
-                tag === "Все темы"
-                  ? courses
-                  : courses.filter((item) =>
-                      item.tags.find((el: any) =>
-                        tag === "Путешествия"
-                          ? el === "Страны и столицы"
-                          : el === tag
-                      )
-                    )
-              );
             }}
             {...(selectedTag === tag && { className: "selected" })}
           >
@@ -67,32 +58,17 @@ function App() {
         ))}
       </ul>
       <div className="courses">
-        <div className="row"> </div>
-        {rows.map((row, index) => (
+        {(selectedTag
+          ? courses.filter((item) => item.tags.includes(selectedTag))
+          : courses
+        ).map((course) => (
           <div
-            key={index}
-            className="row"
-            {...(row.find((course: any) => course.name.length > 13) && {
-              className: "row tallrow",
-            })}
+            key={course.id}
+            className="course"
+            style={{ backgroundColor: course.bgColor }}
           >
-            {row.map((course: any) => (
-              <div
-                key={course.id}
-                className="course"
-                style={{ backgroundColor: course.bgColor }}
-              >
-                <img src={course.image} alt="картинка для курса" />
-                <span>{course.name}</span>
-              </div>
-            ))}
-            {/* скрытые блоки курсов */}
-            {row.length < 3 && (
-              <div className="course" style={{ opacity: 0 }}></div>
-            )}
-            {row.length < 2 && (
-              <div className="course" style={{ opacity: 0 }}></div>
-            )}
+            <img src={course.image} alt="картинка для курса" />
+            <span>{course.name}</span>
           </div>
         ))}
       </div>
